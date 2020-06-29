db1dc5c4fd31187cf9fd5a1c3eb7bf9f466bb998
Sm1te <67575139+Sm1te@users.noreply.github.com>
2020-06-29 15:19:01 +0800

    Add files via upload

---
+++ capstone project.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# this is the new project I have just created.
+
+# In[18]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[ ]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
